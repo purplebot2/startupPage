620c4e542f2975468f733aaebe3ab570be0a5f63
purplebot2 <vancampquinten7@gmail.com>
2026-06-13 22:11:48 +0200

    themes

---
+++ theme.js
@@ -0,0 +1,17 @@
+var themes = ["Q-ocean", "sakura", "lilClaw", "vdb"];
+var themeLink = document.getElementById("theme-link");
+var dropdown = document.getElementById("theme-dropdown");
+
+var themeIndex = Number(localStorage.getItem("themeIndex")) || 0;
+dropdown.selectedIndex = themeIndex;
+setTheme(themes[themeIndex]);
+
+dropdown.addEventListener("change", function () {
+  var index = dropdown.selectedIndex;
+  setTheme(themes[index]);
+  localStorage.setItem("themeIndex", index);
+});
+
+function setTheme(theme) {
+  themeLink.href = "Themes/" + theme + ".css";
+}
